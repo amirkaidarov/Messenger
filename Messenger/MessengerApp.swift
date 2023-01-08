05f889c5b858d61b8289fdb420189bd0507172c7
amirkaidarov <KaidarovAmir1441@gmail.com>
2023-01-08 15:25:38 -0500

    Initial Commit

---
+++ Messenger/MessengerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessengerApp.swift
+//  Messenger
+//
+//  Created by Амир Кайдаров on 1/8/23.
+//
+
+import SwiftUI
+
+@main
+struct MessengerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
